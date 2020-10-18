61852c9c09a82ce84deb51a9c217064a974394fe
you <1738743304@qq.com>
2020-10-18 13:03:38 +0800

    测试

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // 撒旦是的哈就是
+        // 撒旦是的哈就是奥术大师
         setContentView(R.layout.activity_main);
     }
 }
